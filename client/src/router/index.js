321265f0cefbaa8fce00d0c7ae505152458b5ddb
Ryan Rapini <ryanrapini@gmail.com>
2018-10-16 22:45:04 -0400

    adding database support

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
+import EditPost from '@/components/EditPost'
 
 Vue.use(Router)
 
@@ -17,6 +19,16 @@ export default new Router({
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/posts/new',
+      name: 'NewPost',
+      component: NewPost
+    },
+    {
+      path: '/posts/:id',
+      name: 'EditPost',
+      component: EditPost
     }
   ]
 })
